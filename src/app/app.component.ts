f9eeb7334d952c3b14bf24ea9f6d9fe92f21bdb1
woodylewis <woody@woodylewis.com>
2017-09-13 19:21:05 -0400

    wip

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,9 +11,7 @@ export class AppComponent {
   set: Array<any> = [];
   key: string;
 
-  //constructor(public input: string) {
-  constructor() {
-  }
+  constructor() {}
 
   public ngOnInit(): void {
     this.set = this.record.split(/,()/);  
