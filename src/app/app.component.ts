54e9135ccd0ff823c877c71f369154488101f3e3
woodylewis <woody@woodylewis.com>
2017-09-14 07:24:15 -0400

    wip

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,13 +14,29 @@ export class AppComponent {
   constructor() {}
 
   public ngOnInit(): void {
+    let theStr: string = 'foo';
+    let copies: Array<any> = [];
     this.set = this.record.split(/,()/);  
     this.set = this.set.filter((currentValue) => {
       return currentValue.length > 0; 
     })
-    .map((currentValue) => {
-      let theStr: string = currentValue;
-      return currentValue + ' ' + 'foo';
+    .map((currentValue, index, array) => {
+      if(currentValue.includes(array[0])) {
+        let separate: Array<any> = currentValue.split(APP_CONFIG.front);
+        if (separate[0].length > 0) {
+          //copies.push(separate[0]);
+          copies[index] = separate[0];
+          console.log(separate);
+        }
+        currentValue = separate[1];
+      }
+      return currentValue;
+    }, this);
+    console.log(this.set);
+    copies.forEach((key, value) => {
+      console.log(key + ' ' + value);
+      //this.set.splice(this.set[0], 1, key);
+      this.set.splice(value, 1, key);
     }, this);
     console.log(this.set);
   }
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,7 +1,11 @@
 export interface Config {
     input: string;
+    front: string;
+    back: string;
 }
 
 export const APP_CONFIG: Config = {
-    input: '(id,created,employee(id,firstname,employeeType(id), lastname),location)'
+    input: '(id,created,employee(id,firstname,employeeType(id), lastname),location)',
+    front: '(',
+    back: ')'
 };
