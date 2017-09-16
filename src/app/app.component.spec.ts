33bc07c4fbf3c588a3fa2e0bd26b29cfcae3e6b2
woodylewis <woody@woodylewis.com>
2017-09-16 13:27:09 -0400

    Add tests and initial style

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,42 +1,38 @@
-import { TestBed, async } from '@angular/core/testing';
-
+import { ComponentFixture, TestBed, async } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { DebugElement } from '@angular/core';
 import { AppComponent } from './app.component';
 
-describe('AppComponent', () => {
-  beforeEach(async(() => {
+describe('String conversion client', () => {
+  let fixture, app;
+  
+  beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [
-        AppComponent
-      ],
-    }).compileComponents();
-  }));
+      declarations: [ AppComponent ]
+    });
+    fixture = TestBed.createComponent(AppComponent);
+    app = fixture.componentInstance;
+  });
 
-  it('should create the app', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app).toBeTruthy();
-  }));
-  it(`should initialize the input string `, async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.inputRecord).toEqual('(id,created,employee(id,firstname,employeeType(id), lastname),location)');
-  }));
-  it(`should generate an array of strings`, async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.set).toBeArrayOfStrings();
-  }));
-  it(`should generate an array of 9 elements`, async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.set).toBeArrayOfSize(9);
-  }));
-  /*
-  it('should render title in a h1 tag', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
+  it('input record should initialize', async(() => {
+    let i = '(id,created,employee(id,firstname,employeeType(id), lastname),location)';
     fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to app!');
+    fixture.whenStable().then(() => {
+      expect(app.inputRecord).toEqual(i);
+    });
   }));
-*/
+  it('should generate an array of strings', async(() => {
+    fixture.detectChanges();
+    fixture.whenStable().then(() => {
+      expect(app.set).toBeArrayOfStrings();
+    });
+  }));
+  it('array should have 9 elements', async(() => {
+    fixture.detectChanges();
+    fixture.whenStable().then(() => {
+      expect(app.set).toBeArrayOfSize(9);
+    });
+  }));
+  
 });
+
